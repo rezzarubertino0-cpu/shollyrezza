0c084457be691425629e035502b64256ff34b324
sholly rezza r <rezzarubertino0@gmail.com>
2026-05-08 13:22:18 +0700

    update

--- data_produk.php
+++ data_produk.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Data Produk - rezzaproject</title>
+  <title>Data Produk - shollyrezza</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
--- kategori_produk.php
+++ kategori_produk.php
@@ -7,7 +7,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>ategori produk - shollyrezza</title>
+  <title>kategori produk - shollyrezza</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
