86ac855e4f3849d724ee43dced348fd450ebda3e
sholly rezza r <rezzarubertino0@gmail.com>
2026-05-22 14:53:09 +0700

    ubah logo dan profil

--- login.php
+++ login.php
@@ -51,7 +51,7 @@ if (isset($_POST['login'])) {
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/rezza.png" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
@@ -91,7 +91,7 @@ if (isset($_POST['login'])) {
 
               <div class="d-flex justify-content-center py-4">
                 <a href="index.html" class="logo d-flex align-items-center w-auto">
-                  <img src="assets/img/logo.png" alt="">
+                  <img src="assets/img/rezza.png" alt="">
                   <span class="d-none d-lg-block">shollyrezza</span>
                 </a>
               </div><!-- End Logo -->
